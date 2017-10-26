a621ec1f5460f3a7515a472e9295632153ca2f77
zeinajnaydi <30489870+zeinajnaydi@users.noreply.github.com>
2017-10-26 13:39:29 +0300

    update sum.test

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(4);
+// });
+
+describe('addition', () => {
+  describe('positive numbers', () => {
+    it('adds 1 + 2 to equal 3', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
 });
