7868ed47a52aee9e63002d1febf9f34a75b938f7
zeinajnaydi <30489870+zeinajnaydi@users.noreply.github.com>
2017-10-26 13:51:15 +0300

    change from addition ti addition test

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -4,7 +4,7 @@ const sum = require('./sum');
 //   expect(sum(1, 2)).toBe(4);
 // });
 
-describe('addition', () => {
+describe('addition test', () => {
   describe('positive numbers', () => {
     it('adds 1 + 2 to equal 3', () => {
       expect(sum(1, 2)).toBe(3);
